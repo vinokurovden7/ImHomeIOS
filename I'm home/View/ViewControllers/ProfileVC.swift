d0fe42f1934730ed55926e6e650c19b9705b1421
Denis <vinokurovden7@gmail.com>
2020-05-30 17:50:37 +0500

    Переделал окно регистрации и экран редактирования профиля на UITableView

--- I'm home/View/ViewControllers/ProfileVC.swift
+++ I'm home/View/ViewControllers/ProfileVC.swift
@@ -8,125 +8,33 @@
 
 import UIKit
 
-class ProfileVC: UIViewController {
+class ProfileVC: UITableViewController {
 
     @IBOutlet weak var emailTextField: UITextField! {
         didSet {
-            //Basic texfield Setup
-            emailTextField.borderStyle = .none
-            emailTextField.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            emailTextField.layer.cornerRadius = emailTextField.frame.size.height / 2
-
-            //To apply border
-            emailTextField.layer.borderWidth = 0.25
-            emailTextField.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            emailTextField.layer.shadowOpacity = 0.2
-            emailTextField.layer.shadowRadius = 5.0
-            emailTextField.layer.shadowOffset = CGSize.zero // Use any CGSize
-            emailTextField.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: emailTextField.frame.height))
-            emailTextField.leftView = paddingView
-            emailTextField.leftViewMode = UITextField.ViewMode.always
-            emailTextField.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            emailTextField.layer.shadowOffset = CGSize(width: 0, height: 4)
-            emailTextField.layer.shadowColor = UIColor.black.cgColor //Any dark color
-            emailTextField.overrideUserInterfaceStyle = .light
+            emailTextField.attributedPlaceholder = NSAttributedString(string: "Введите Email",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
     
     @IBOutlet weak var secondNameTextField: UITextField! {
         didSet {
-            //Basic texfield Setup
-            secondNameTextField.borderStyle = .none
-            secondNameTextField.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            secondNameTextField.layer.cornerRadius = secondNameTextField.frame.size.height / 2
-
-            //To apply border
-            secondNameTextField.layer.borderWidth = 0.25
-            secondNameTextField.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            secondNameTextField.layer.shadowOpacity = 0.2
-            secondNameTextField.layer.shadowRadius = 5.0
-            secondNameTextField.layer.shadowOffset = CGSize.zero // Use any CGSize
-            secondNameTextField.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: secondNameTextField.frame.height))
-            secondNameTextField.leftView = paddingView
-            secondNameTextField.leftViewMode = UITextField.ViewMode.always
-            secondNameTextField.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            secondNameTextField.layer.shadowOffset = CGSize(width: 0, height: 4)
-            secondNameTextField.layer.shadowColor = UIColor.black.cgColor //Any dark color
-            secondNameTextField.overrideUserInterfaceStyle = .light
+            secondNameTextField.attributedPlaceholder = NSAttributedString(string: "Введите фамилию",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
     
     @IBOutlet weak var firstNameTextField: UITextField! {
         didSet {
-            //Basic texfield Setup
-            firstNameTextField.borderStyle = .none
-            firstNameTextField.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            firstNameTextField.layer.cornerRadius = firstNameTextField.frame.size.height / 2
-
-            //To apply border
-            firstNameTextField.layer.borderWidth = 0.25
-            firstNameTextField.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            firstNameTextField.layer.shadowOpacity = 0.2
-            firstNameTextField.layer.shadowRadius = 5.0
-            firstNameTextField.layer.shadowOffset = CGSize.zero // Use any CGSize
-            firstNameTextField.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: firstNameTextField.frame.height))
-            firstNameTextField.leftView = paddingView
-            firstNameTextField.leftViewMode = UITextField.ViewMode.always
-            firstNameTextField.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            firstNameTextField.layer.shadowOffset = CGSize(width: 0, height: 4)
-            firstNameTextField.layer.shadowColor = UIColor.black.cgColor //Any dark color
-            firstNameTextField.overrideUserInterfaceStyle = .light
+            firstNameTextField.attributedPlaceholder = NSAttributedString(string: "Введите имя",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
     
     @IBOutlet weak var thirdNameTextField: UITextField! {
         didSet {
-            //Basic texfield Setup
-            thirdNameTextField.borderStyle = .none
-            thirdNameTextField.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            thirdNameTextField.layer.cornerRadius = thirdNameTextField.frame.size.height / 2
-
-            //To apply border
-            thirdNameTextField.layer.borderWidth = 0.25
-            thirdNameTextField.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            thirdNameTextField.layer.shadowOpacity = 0.2
-            thirdNameTextField.layer.shadowRadius = 5.0
-            thirdNameTextField.layer.shadowOffset = CGSize.zero // Use any CGSize
-            thirdNameTextField.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: thirdNameTextField.frame.height))
-            thirdNameTextField.leftView = paddingView
-            thirdNameTextField.leftViewMode = UITextField.ViewMode.always
-            thirdNameTextField.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            thirdNameTextField.layer.shadowOffset = CGSize(width: 0, height: 4)
-            thirdNameTextField.layer.shadowColor = UIColor.black.cgColor //Any dark color
-            thirdNameTextField.overrideUserInterfaceStyle = .light
+            thirdNameTextField.attributedPlaceholder = NSAttributedString(string: "Введите отчество",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
     
@@ -136,11 +44,7 @@ class ProfileVC: UIViewController {
         }
     }
     
-    @IBOutlet weak var saveBtn: UIButton! {
-        didSet {
-            saveBtn.layer.cornerRadius = saveBtn.frame.height / 2
-        }
-    }
+    @IBOutlet weak var saveBtn: UIBarButtonItem!
     
     @IBOutlet weak var deleteAccountBtn: UIButton! {
         didSet {
@@ -148,14 +52,7 @@ class ProfileVC: UIViewController {
         }
     }
     
-    @IBOutlet weak var imageProfile: UIImageView! {
-        didSet {
-            imageProfile.layer.cornerRadius = 20
-            imageProfile.layer.borderWidth = 2
-            imageProfile.layer.borderColor = UIColor.white.cgColor
-            imageProfile.clipsToBounds = true
-        }
-    }
+    @IBOutlet weak var imageProfile: UIImageView! 
     
     @IBOutlet weak var backgroundViewProfileImage: UIView! {
         didSet {
@@ -186,7 +83,10 @@ class ProfileVC: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-    @IBAction func saveBtnAction(_ sender: UIButton) {
+    @IBAction func saveBtnAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    @IBAction func cancelBtnAction(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
     
--- I'm home/View/ViewControllers/RegistrationVC.swift
+++ I'm home/View/ViewControllers/RegistrationVC.swift
@@ -8,201 +8,46 @@
 
 import UIKit
 
-class RegistrationVC: UIViewController {
+class RegistrationVC: UITableViewController {
 
-    @IBOutlet var topConstraint: NSLayoutConstraint!
     @IBOutlet var emailTextField: UITextField! {
         didSet {
-            //Basic texfield Setup
-            emailTextField.borderStyle = .none
-            emailTextField.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            emailTextField.layer.cornerRadius = emailTextField.frame.size.height / 2
-
-            //To apply border
-            emailTextField.layer.borderWidth = 0.25
-            emailTextField.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            emailTextField.layer.shadowOpacity = 0.2
-            emailTextField.layer.shadowRadius = 5.0
-            emailTextField.layer.shadowOffset = CGSize.zero // Use any CGSize
-            emailTextField.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: emailTextField.frame.height))
-            emailTextField.leftView = paddingView
-            emailTextField.leftViewMode = UITextField.ViewMode.always
-            emailTextField.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            emailTextField.layer.shadowOffset = CGSize(width: 0, height: 4)
-            emailTextField.layer.shadowColor = UIColor.black.cgColor //Any dark color
-//            let spacerView = UIView(frame:CGRect(x:0, y:0, width:40, height:10))
-//            emailTextField.leftViewMode = UITextField.ViewMode.always
-//            emailTextField.leftView = spacerView
-            emailTextField.overrideUserInterfaceStyle = .light
+            emailTextField.attributedPlaceholder = NSAttributedString(string: "Введите Email",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
     @IBOutlet var usernameTextField: UITextField! {
         didSet {
-            //Basic texfield Setup
-            usernameTextField.borderStyle = .none
-            usernameTextField.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            usernameTextField.layer.cornerRadius = usernameTextField.frame.size.height / 2
-
-            //To apply border
-            usernameTextField.layer.borderWidth = 0.25
-            usernameTextField.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            usernameTextField.layer.shadowOpacity = 0.2
-            usernameTextField.layer.shadowRadius = 5.0
-            usernameTextField.layer.shadowOffset = CGSize.zero // Use any CGSize
-            usernameTextField.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: usernameTextField.frame.height))
-            usernameTextField.leftView = paddingView
-            usernameTextField.leftViewMode = UITextField.ViewMode.always
-            usernameTextField.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            usernameTextField.layer.shadowOffset = CGSize(width: 0, height: 4)
-            usernameTextField.layer.shadowColor = UIColor.black.cgColor //Any dark color
-//            let spacerView = UIView(frame:CGRect(x:0, y:0, width:40, height:10))
-//            usernameTextField.leftViewMode = UITextField.ViewMode.always
-//            usernameTextField.leftView = spacerView
-            usernameTextField.overrideUserInterfaceStyle = .light
+            usernameTextField.attributedPlaceholder = NSAttributedString(string: "Введите имя пользователя",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
     @IBOutlet var passwordTextField: UITextField! {
         didSet {
-            //Basic texfield Setup
-            passwordTextField.borderStyle = .none
-            passwordTextField.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            passwordTextField.layer.cornerRadius = passwordTextField.frame.size.height / 2
-
-            //To apply border
-            passwordTextField.layer.borderWidth = 0.25
-            passwordTextField.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            passwordTextField.layer.shadowOpacity = 0.2
-            passwordTextField.layer.shadowRadius = 5.0
-            passwordTextField.layer.shadowOffset = CGSize.zero // Use any CGSize
-            passwordTextField.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: passwordTextField.frame.height))
-            passwordTextField.leftView = paddingView
-            passwordTextField.leftViewMode = UITextField.ViewMode.always
-            passwordTextField.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            passwordTextField.layer.shadowOffset = CGSize(width: 0, height: 4)
-            passwordTextField.layer.shadowColor = UIColor.black.cgColor //Any dark color
-//            let spacerView = UIView(frame:CGRect(x:0, y:0, width:40, height:10))
-//            passwordTextField.leftViewMode = UITextField.ViewMode.always
-//            passwordTextField.leftView = spacerView
-            passwordTextField.overrideUserInterfaceStyle = .light
+            passwordTextField.attributedPlaceholder = NSAttributedString(string: "Введите пароль",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
     @IBOutlet weak var secondNameUser: UITextField! {
         didSet {
-            //Basic texfield Setup
-            secondNameUser.borderStyle = .none
-            secondNameUser.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            secondNameUser.layer.cornerRadius = secondNameUser.frame.size.height / 2
-
-            //To apply border
-            secondNameUser.layer.borderWidth = 0.25
-            secondNameUser.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            secondNameUser.layer.shadowOpacity = 0.2
-            secondNameUser.layer.shadowRadius = 5.0
-            secondNameUser.layer.shadowOffset = CGSize.zero // Use any CGSize
-            secondNameUser.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: secondNameUser.frame.height))
-            secondNameUser.leftView = paddingView
-            secondNameUser.leftViewMode = UITextField.ViewMode.always
-            secondNameUser.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            secondNameUser.layer.shadowOffset = CGSize(width: 0, height: 4)
-            secondNameUser.layer.shadowColor = UIColor.black.cgColor //Any dark color
-//            let spacerView = UIView(frame:CGRect(x:0, y:0, width:40, height:10))
-//            secondNameUser.leftViewMode = UITextField.ViewMode.always
-//            secondNameUser.leftView = spacerView
-            secondNameUser.overrideUserInterfaceStyle = .light
+            secondNameUser.attributedPlaceholder = NSAttributedString(string: "Введите фамилию",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
     @IBOutlet weak var firstNameUser: UITextField! {
         didSet {
-            //Basic texfield Setup
-            firstNameUser.borderStyle = .none
-            firstNameUser.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            firstNameUser.layer.cornerRadius = firstNameUser.frame.size.height / 2
-
-            //To apply border
-            firstNameUser.layer.borderWidth = 0.25
-            firstNameUser.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            firstNameUser.layer.shadowOpacity = 0.2
-            firstNameUser.layer.shadowRadius = 5.0
-            firstNameUser.layer.shadowOffset = CGSize.zero // Use any CGSize
-            firstNameUser.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: firstNameUser.frame.height))
-            firstNameUser.leftView = paddingView
-            firstNameUser.leftViewMode = UITextField.ViewMode.always
-            firstNameUser.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            firstNameUser.layer.shadowOffset = CGSize(width: 0, height: 4)
-            firstNameUser.layer.shadowColor = UIColor.black.cgColor //Any dark color
-//            let spacerView = UIView(frame:CGRect(x:0, y:0, width:40, height:10))
-//            firstNameUser.leftViewMode = UITextField.ViewMode.always
-//            firstNameUser.leftView = spacerView
-            firstNameUser.overrideUserInterfaceStyle = .light
+            firstNameUser.attributedPlaceholder = NSAttributedString(string: "Введите имя",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
     @IBOutlet weak var thirdNameUser: UITextField! {
         didSet {
-            //Basic texfield Setup
-            thirdNameUser.borderStyle = .none
-            thirdNameUser.backgroundColor = .white // Use anycolor that give you a 2d look.
-
-            //To apply corner radius
-            thirdNameUser.layer.cornerRadius = thirdNameUser.frame.size.height / 2
-
-            //To apply border
-            thirdNameUser.layer.borderWidth = 0.25
-            thirdNameUser.layer.borderColor = UIColor.white.cgColor
-
-            //To apply Shadow
-            thirdNameUser.layer.shadowOpacity = 0.2
-            thirdNameUser.layer.shadowRadius = 5.0
-            thirdNameUser.layer.shadowOffset = CGSize.zero // Use any CGSize
-            thirdNameUser.layer.shadowColor = UIColor.gray.cgColor
-
-            //To apply padding
-            let paddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: thirdNameUser.frame.height))
-            thirdNameUser.leftView = paddingView
-            thirdNameUser.leftViewMode = UITextField.ViewMode.always
-            thirdNameUser.layer.borderColor = UIColor.black.withAlphaComponent(0.25).cgColor
-            thirdNameUser.layer.shadowOffset = CGSize(width: 0, height: 4)
-            thirdNameUser.layer.shadowColor = UIColor.black.cgColor //Any dark color
-//            let spacerView = UIView(frame:CGRect(x:0, y:0, width:40, height:10))
-//            thirdNameUser.leftViewMode = UITextField.ViewMode.always
-//            thirdNameUser.leftView = spacerView
-            thirdNameUser.overrideUserInterfaceStyle = .light
+            thirdNameUser.attributedPlaceholder = NSAttributedString(string: "Введите отчество",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         }
     }
+    @IBOutlet weak var goLoginScreenBtn: UIButton!
+    @IBOutlet weak var imageAccount: UIImageView!
     @IBOutlet weak var registrationBtn: UIButton! {
         didSet {
             registrationBtn.layer.cornerRadius = registrationBtn.frame.height / 2
@@ -216,6 +61,9 @@ class RegistrationVC: UIViewController {
 
     //MARK: Обработчики
     @IBAction func loginBtnAction(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    @IBAction func goLoginScreenBtnAction(_ sender: UIButton) {
         dismiss(animated: true)
     }
     
